a439235c12248fdba92f01a3153bbd158760aa28
Lu73-star <992097617luciano@gmail.com>
2025-11-21 19:08:11 -0300

    Add SplashScreen widget with fade-in animation

---
+++ lib/splash_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import '../theme/app_theme.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _opacityAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
+    );
+
+    _controller.forward();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppTheme.backgroundColor,
+      body: Center(
+        child: FadeTransition(
+          opacity: _opacityAnimation,
+          child: Image.asset(
+            'assets/images/logo_voxfuture.png',
+            width: 260,
+            fit: BoxFit.contain,
+          ),
+        ),
+      ),
+    );
+  }
