4f19406140d98a72e3a8d351af65e955cc0248ca
Lu73-star <992097617luciano@gmail.com>
2025-11-24 13:35:07 -0300

    Update app_navigation.dart

--- lib/navigation/lib/navigation/app_navigation.dart
+++ lib/navigation/lib/navigation/app_navigation.dart
@@ -1,35 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:voxfuture_flutterclean/screens/splash_screen.dart';
-import 'package:voxfuture_flutterclean/screens/home_screen.dart';
-import 'package:voxfuture_flutterclean/screens/login_screen.dart';
-import 'package:voxfuture_flutterclean/screens/register_screen.dart';
+import '../screens/login_screen.dart';
+import '../screens/register_screen.dart';
+import '../screens/home_screen.dart';
+import '../splash_screen.dart';
 
 class AppNavigation {
-  static Route<dynamic> generateRoute(RouteSettings settings) {
-    switch (settings.name) {
-      case '/':
-        return MaterialPageRoute(builder: (_) => const SplashScreen());
+  // Rotas nomeadas
+  static const String rotaSplash = '/';
+  static const String rotaLogin = '/login';
+  static const String rotaCadastro = '/cadastro';
+  static const String rotaHome = '/home';
 
-      case '/home':
-        return MaterialPageRoute(builder: (_) => const HomeScreen());
+  // Mapa de rotas
+  static Map<String, WidgetBuilder> rotas = {
+    rotaSplash: (context) => const TelaInicial(),
+    rotaLogin: (context) => const LoginScreen(),
+    rotaCadastro: (context) => const RegisterScreen(),
+    rotaHome: (context) => const HomeScreen(),
+  };
 
-      case '/login':
-        return MaterialPageRoute(builder: (_) => const LoginScreen());
+  // Navegações rápidas
+  static void irParaLogin(BuildContext context) {
+    Navigator.pushReplacementNamed(context, rotaLogin);
+  }
 
-      case '/register':
-        return MaterialPageRoute(builder: (_) => const RegisterScreen());
+  static void irParaCadastro(BuildContext context) {
+    Navigator.pushNamed(context, rotaCadastro);
+  }
 
-      default:
-        return MaterialPageRoute(
-          builder: (_) => const Scaffold(
-            body: Center(
-              child: Text(
-                'Rota não encontrada',
-                style: TextStyle(color: Colors.white, fontSize: 18),
-              ),
-            ),
-          ),
-        );
-    }
+  static void irParaHome(BuildContext context) {
+    Navigator.pushReplacementNamed(context, rotaHome);
   }
 }
