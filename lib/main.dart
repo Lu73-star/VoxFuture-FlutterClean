186c483ea5a9d51dfd2748d8008d1c5f4ceb4fd2
Lu73-star <992097617luciano@gmail.com>
2025-11-29 22:28:30 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
 import 'config/app_theme.dart';
-import 'config/routes.dart';
-import 'screens/auth_wrapper.dart';
-import 'services/auth_service.dart';
-import 'package:provider/provider.dart';
+import 'screens/home_page.dart';
 
-import 'firebase_options.dart'; // será gerado após você enviar o JSON
-
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -22,17 +19,11 @@ class VoxFutureApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<AuthService>(create: (_) => AuthService()),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'VoxFuture',
-        theme: AppTheme.darkTheme,
-        routes: AppRoutes.routes,
-        home: const AuthWrapper(),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'VoxFuture',
+      theme: AppTheme.darkTheme,
+      home: const HomePage(),
     );
   }
 }
