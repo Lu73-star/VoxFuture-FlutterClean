ac19e64ecf4e98439ddbcd587af1b7afd2716a75
Lu73-star <992097617luciano@gmail.com>
2025-11-29 22:08:39 -0300

    Create register_page.dart

---
+++ lib/screens/register_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../services/auth_service.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final email = TextEditingController();
+  final pass = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Criar conta')),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: email,
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: pass,
+              decoration: const InputDecoration(labelText: 'Senha'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                try {
+                  await context.read<AuthService>().register(
+                        email.text.trim(),
+                        pass.text.trim(),
+                      );
+                  Navigator.pushReplacementNamed(context, '/home');
+                } catch (e) {
+                  debugPrint(e.toString());
+                }
+              },
+              child: const Text('Registrar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
