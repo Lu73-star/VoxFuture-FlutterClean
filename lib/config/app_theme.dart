6114896b6d59837d978e41b32b6d7325a80a7856
Lu73-star <992097617luciano@gmail.com>
2025-11-29 21:54:16 -0300

    Update app_theme.dart

--- lib/config/app_theme.dart
+++ lib/config/app_theme.dart
@@ -5,11 +5,22 @@ class AppTheme {
     brightness: Brightness.dark,
     primaryColor: const Color(0xFF0A0F28),
     scaffoldBackgroundColor: const Color(0xFF0A0F28),
+
     colorScheme: const ColorScheme.dark(
-      primary: Color(0xFF1A224F),
-      secondary: Color(0xFFD4AF37), // dourado
+      primary: Color(0xFF1A224F),     // azul escuro
+      secondary: Color(0xFFFDD4A3),   // dourado suave
     ),
+
     fontFamily: 'Roboto',
-    useMaterial3: true,
+
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color(0xFF0A0F28),
+      centerTitle: true,
+      titleTextStyle: TextStyle(
+        color: Colors.white,
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
   );
 }
