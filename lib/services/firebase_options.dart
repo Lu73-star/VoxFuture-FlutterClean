7e8c7099eef81541b85325504adb9eaddc511176
Lu73-star <992097617luciano@gmail.com>
2025-12-01 14:06:55 -0300

    Update firebase_options.dart

--- lib/services/firebase_options.dart
+++ lib/services/firebase_options.dart
@@ -1,37 +1,52 @@
 // File: lib/services/firebase_options.dart
 
-import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart'
-    show defaultTargetPlatform, TargetPlatform, kIsWeb;
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'A configuração Web ainda não foi adicionada. '
-        'Registre o app Web no Firebase e envie a config.',
-      );
+      return web;
     }
 
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return android;
+
       case TargetPlatform.iOS:
         throw UnsupportedError(
-          'Configuração iOS não encontrada. Adicione para ativar no iPhone.',
+          'Configuração iOS não encontrada.',
         );
+
       default:
         throw UnsupportedError(
-          'Plataforma não suportada para Firebase.');
+          'Plataforma não suportada pelo Firebase.',
+        );
     }
   }
 
+  // ------------------------------------------------------------
   // ANDROID
+  // ------------------------------------------------------------
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: "AIzaSyA-2VvvLbrYF0xvm237XqV7uZttsQMlj9c",
     appId: "1:957218435739:android:fdb486ad33baa37bcdcc3e",
     messagingSenderId: "957218435739",
     projectId: "voxfuture-12d52",
     storageBucket: "voxfuture-12d52.firebasestorage.app",
+  );
+
+  // ------------------------------------------------------------
+  // WEB
+  // ------------------------------------------------------------
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: "AIzaSyDUDiuIYmPCf4_KjKqkgZz43ym5oVio-EA",
+    authDomain: "voxfuture-12d52.firebaseapp.com",
+    projectId: "voxfuture-12d52",
+    storageBucket: "voxfuture-12d52.firebasestorage.app",
+    messagingSenderId: "957218435739",
+    appId: "1:957218435739:web:0dba10898b7cbb61cdcc3e",
+    measurementId: "G-BBPEBEZY6H",
   );
 }
